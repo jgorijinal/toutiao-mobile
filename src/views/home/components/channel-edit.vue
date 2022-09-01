13a0a42f7e2832be7959b600cd24579af6cc24a6
jgoriginal <932274204@qq.com>
2022-09-01 15:11:46 +0800

    编辑频道页面样式

--- src/views/home/components/channel-edit.vue
+++ src/views/home/components/channel-edit.vue
@@ -3,21 +3,21 @@
     <!--我的频道部分-->
     <van-cell :border="false">
       <template #title>
-        我的频道
+        <span class="title-text">我的频道</span>
       </template>
-      <van-button size="mini" type="danger" plain round>编辑</van-button>
+      <van-button class="edit-btn" size="mini" type="danger" plain round>编辑</van-button>
     </van-cell>
-    <van-grid>
-      <van-grid-item v-for="value in 8" :key="value" text="文字" />
+    <van-grid gutter="10" class="my-grid">
+      <van-grid-item class="grid-item" v-for="value in 8" :key="value" text="文字" icon="close"/>
     </van-grid>
     <!--推荐频道部分-->
     <van-cell :border="false">
       <template #title>
-        频道推荐
+        <span class="title-text">频道推荐</span>
       </template>
     </van-cell>
-    <van-grid>
-      <van-grid-item v-for="value in 8" :key="value" text="文字" />
+    <van-grid gutter="10" class="recommend-grid">
+      <van-grid-item icon="plus" class="grid-item" v-for="value in 8" :key="value" text="文字" />
     </van-grid>
   </div>
 </template>
@@ -28,8 +28,54 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less" scoped>
 .channel-edit {
   padding: 85px 0;
+  .title-text {
+    font-size:32px;
+    color: #333333;
+  }
+  .edit-btn {
+    width:104px;
+    height:48px;
+    font-size:26px;
+  }
+  /deep/.grid-item {
+    width:160px;
+    height:86px;
+    .van-grid-item__content{
+      background-color: #f4f5f6;
+      white-space: nowrap;
+      border-radius: 6px;
+      .van-grid-item__text{
+        font-size:28px;
+      }
+    }
+  }
+  /deep/ .my-grid {
+    .grid-item {
+      .van-icon-close {
+        position:absolute;
+        right:-6px;
+        top:-6px;
+        z-index:2;
+        font-size:34px;
+      }
+    }
+  }
+  /deep/.recommend-grid {
+    .grid-item {
+      .van-grid-item__content {
+        flex-direction: row;
+        .van-icon-plus {
+          font-size: 28px;
+          margin-right:6px;
+        }
+        .van-grid-item__text {
+          margin-top: 0
+        }
+      }
+    }
+  }
 }
 </style>
