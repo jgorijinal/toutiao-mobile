776993017d43532277adc89f200c9e21267de873
jgoriginal <932274204@qq.com>
2022-09-02 16:19:27 +0800

    匹配关键字高亮

--- src/views/search/components/search-suggestion.vue
+++ src/views/search/components/search-suggestion.vue
@@ -1,12 +1,17 @@
 <template>
   <!--搜索联想建议-->
   <div class="search-suggestion">
-    <van-cell v-for="(item, index) in suggestions" :key="index" :title="item" icon="search"></van-cell>
+    <van-cell v-for="(item, index) in suggestions" :key="index" icon="search">
+      <template #title>
+        <span v-html="highlight(item)"></span>
+      </template>
+    </van-cell>
   </div>
 </template>
 
 <script>
 import { getSuggestions } from '@/api/search'
+import { debounce } from 'lodash'
 export default {
   props: {
     value: {
@@ -21,9 +26,9 @@ export default {
   },
   watch: { // watch 监听父组件传过来的搜索框内容
     value: {
-      handler (newValue) {
+      handler: debounce(function (newValue) {
         this.getSuggestions(newValue)
-      },
+      }, 500),
       immediate: true
     }
   },
@@ -35,6 +40,14 @@ export default {
       } catch (err) {
         this.$toast('获取数据失败, 请重试')
       }
+    },
+    highlight (text) {
+      const highlightString = `<span style="color:red">${this.value}</span>`
+
+      // 如果根据数据变量动态生成正则 , 需要用 new RegExp()
+      // 参数1 : 匹配模式字符串, 根据这个字符串创建正则对象
+      const reg = new RegExp(this.value, 'gi')
+      return text && text.replace(reg, highlightString)
     }
   }
 }
