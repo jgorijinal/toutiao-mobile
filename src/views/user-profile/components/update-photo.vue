7bf60bfaebce03a50d2afb89cebbc237cc0a8a23
jgoriginal <932274204@qq.com>
2022-09-05 15:42:01 +0800

    头像上传预览

---
+++ src/views/user-profile/components/update-photo.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="update-photo">
+    <img class="profile-image" :src="img">
+    <div class="toolbar">
+      <div class="cancel" @click="$emit('click-close')">取消</div>
+      <div class="confirm">确定</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    img: {
+      type: [String, Object],
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.profile-image {
+  max-width:100%
+}
+.update-photo{
+  height:100%;
+  background-color:black ;
+  .toolbar{
+    position:fixed;
+    left:0;
+    right:0;
+    bottom:0;
+    display: flex;
+    justify-content: space-between;
+    .cancel,.confirm {
+      width:90px;
+      height:90px;
+      font-size:30px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      color:#fff
+    }
+  }
+}
+</style>
--- src/views/user-profile/index.vue
+++ src/views/user-profile/index.vue
@@ -2,7 +2,9 @@
   <div class="user-profile">
     <van-nav-bar title="个人信息" left-arrow right-text="保存" @click-left="$router.back()"/>
     <van-cell-group>
-      <van-cell title="头像" is-link>
+      <!--表单元素的 hidden 表示隐藏这个表单元素-->
+      <input type="file" hidden ref="file" @change="onFileChange">
+      <van-cell title="头像" is-link @click="$refs.file.click()">
         <van-image
           round
           width="30"
@@ -42,6 +44,15 @@
       >
       <update-birthday v-if="isUpdateBirthdayShow" v-model="profile.birthday" @click-close="isUpdateBirthdayShow = false"/>
     </van-popup>
+    <!--(修改头像)弹出层-->
+    <van-popup
+      v-model="isUpdatePhotoShow"
+      :style="{height:'100%'}"
+      position="bottom"
+      get-container="body"
+      >
+      <update-photo :img="img" @click-close="isUpdatePhotoShow = false" v-if="isUpdatePhotoShow"></update-photo>
+    </van-popup>
   </div>
 </template>
 
@@ -50,19 +61,23 @@ import { getUserProfile } from '@/api/user'
 import updateName from './components/update-name.vue'
 import updateGender from './components/update-gender.vue'
 import updateBirthday from './components/update-birthday.vue'
+import updatePhoto from './components/update-photo.vue'
 export default {
   name: 'UserProfile',
   components: {
     updateName,
     updateGender,
-    updateBirthday
+    updateBirthday,
+    updatePhoto
   },
   data () {
     return {
       profile: {},
       isUpdateNameShow: false, // 修改昵称弹出层 显示/隐藏
       isUpdateGenderShow: false, // 修改性别弹出层 显示/隐藏
-      isUpdateBirthdayShow: false // 修改生日
+      isUpdateBirthdayShow: false, // 修改生日
+      isUpdatePhotoShow: false, // 修改头像
+      img: null
     }
   },
   created () {
@@ -77,6 +92,19 @@ export default {
       } catch (err) {
         console.log(err)
       }
+    },
+    onFileChange () { // file 的 change事件
+      // 获取文件对象
+      const file = this.$refs.file.files[0]
+      // 基于文件对象获取 blob 数据
+      this.img = window.URL.createObjectURL(file)
+
+      // 选择图片后让弹层出现
+      this.isUpdatePhotoShow = true
+
+      // file-input 如果处理同一个文件就不会触发 change 事件
+      // 解决办法是每次使用完毕, 把他的 value 清空
+      this.$refs.file.value = ''
     }
   }
 }
